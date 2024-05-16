28aede8945f661f70692962e50e60cb587d43859
Mathushan Sivakumaran <sivakmat@students.zhaw.ch>
2024-05-16 20:47:34 +0200

    Test 1 - failed

---
+++ app/src/main/java/org/example/ScoreKeeper.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ScoreKeeper {
+    private int scoreTeamA;
+    private int scoreTeamB;
+
+    public ScoreKeeper() {
+        scoreTeamA = 0;
+        scoreTeamB = 0;
+    }
+}
+
---
+++ app/src/test/java/org/example/ScoreKeeperTest.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ScoreKeeperTest {
+    private ScoreKeeper scoreKeeper;
+
+    @BeforeEach
+    public void setUp() {
+        scoreKeeper = new ScoreKeeper();
+    }
+
+    @@Test
+    public void initialScoresShouldBeZero() {
+        assertEquals(0, scoreKeeper.getScoreTeamA());
+        assertEquals(0, scoreKeeper.getScoreTeamB());
+    }
+
+}
