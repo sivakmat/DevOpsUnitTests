e301526c5faaaf196153adabbac350e126525bb2
Mathushan Sivakumaran <sivakmat@students.zhaw.ch>
2024-05-16 21:02:55 +0200

    Test 2 - passed

--- app/src/main/java/org/example/ScoreKeeper.java
+++ app/src/main/java/org/example/ScoreKeeper.java
@@ -17,6 +17,14 @@ public class ScoreKeeper {
     public int getScoreTeamB() {
         return scoreTeamB;
     }
-    
+
+    public String getScore() {
+        return String.format("%03d:%03d", scoreTeamA, scoreTeamB);
+    }
+
+    public void scoreTeamA1() {
+        scoreTeamA += 1;
+    }
+
 }
 
